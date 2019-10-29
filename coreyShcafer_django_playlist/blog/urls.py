31656a16903cdf00c739f00acf0206de0801be73
AsemAntar <asemantar@gmail.com>
2019-10-29 08:44:48 +0300

    adding create, update and delete views

--- coreyShcafer_django_playlist/blog/urls.py
+++ coreyShcafer_django_playlist/blog/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
+from .views import (
+    PostListViews,
+    PostDetailViews,
+    PostCreateViews,
+    PostUpdateViews,
+    PostDeleteViews
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListViews.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailViews.as_view(), name='post-detail'),
+    path('post/new/', PostCreateViews.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateViews.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteViews.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
