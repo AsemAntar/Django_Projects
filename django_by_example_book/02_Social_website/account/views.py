c8e32be03a207378f3b1d269be8a630163dd71b2
AsemAntar <asemantar@gmail.com>
2020-01-08 14:35:50 +0300

    creating user registeration and forgot password

--- django_by_example_book/02_Social_website/account/views.py
+++ django_by_example_book/02_Social_website/account/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import LoginForm, UserRegisterationForm
 
 
 def user_login(request):
@@ -25,6 +25,23 @@ def user_login(request):
         form = LoginForm()
 
     return render(request, 'account/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegisterationForm(request.POST)
+        if user_form.is_valid():
+            # create new user object but avoid saving it
+            new_user = user_form.save(commit=False)
+            # set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # save the user
+            new_user.save()
+
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegisterationForm()
+        return render(request, 'account/register.html', {'user_form': user_form})
 
 
 @login_required
