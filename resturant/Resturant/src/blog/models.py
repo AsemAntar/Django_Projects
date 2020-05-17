fdc493614925c03efa5dfccdf58ab55546f19d3a
AsemAntar <asemantar@gmail.com>
2020-05-17 13:09:44 +0300

    Adding Comments

--- resturant/Resturant/src/blog/models.py
+++ resturant/Resturant/src/blog/models.py
@@ -28,3 +28,13 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'A comment for {self.post.title}'
--- resturant/Resturant/src/blog/views.py
+++ resturant/Resturant/src/blog/views.py
@@ -1,8 +1,8 @@
-from django.db.models import Count
-from django.shortcuts import render
-from .models import Post, Category
-
 from taggit.models import Tag
+from django.db.models import Count
+from django.shortcuts import render, redirect
+from .models import Post, Category, Comment
+from.forms import CommentForm
 
 
 def post_list(request):
@@ -17,11 +17,30 @@ def post_detail(request, id):
     post_detail = Post.objects.get(id=id)
     categories = Category.objects.all().annotate(posts_count=Count('post'))
     all_tags = Tag.objects.all()
+    comments = Comment.objects.filter(post=post_detail).order_by('-created')
+    comments_count = Comment.objects.filter(post=post_detail).count()
+
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = post_detail
+            new_comment.save()
+        return redirect('blog:post_detail', id=id)
+    else:
+        comment_form = CommentForm()
 
     context = {
         'post_detail': post_detail,
         'categories': categories,
         'all_tags': all_tags,
+        'comments': comments,
+        'comments_count': comments_count,
+        'new_comment': new_comment,
+        'comment_form': comment_form,
     }
     return render(request, 'blog/post_detail.html', context)
 
